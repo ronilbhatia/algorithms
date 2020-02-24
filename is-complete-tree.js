4dcf9064f3e9a1960bd686f64f9fc8f6620759d4
Ronil Bhatia <ronilbhatia@gmail.com>
2020-02-23 21:08:22 -0800

    Add is complete tree

---
+++ is-complete-tree.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isCompleteTree = function (root) {
+  if (!root) return false;
+
+  let queue = [root];
+  let hitNull = false;
+  while (queue.length) {
+    let curr = queue.shift();
+    if (!curr) {
+      hitNull = true;
+      continue;
+    } else if (hitNull) return false;
+
+    queue.push(curr.left);
+    queue.push(curr.right);
+  }
+
+  return this;
+};
