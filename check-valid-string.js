e37aa6b1dd6b0035b9866f0367ab803f6bd47937
ronilbhatia <ronilbhatia@gmail.com>
2020-04-16 22:43:21 -0700

    Add check valid string one iteration

--- check-valid-string.js
+++ check-valid-string.js
@@ -2,6 +2,27 @@
  * @param {string} s
  * @return {boolean}
  */
+// Single iteration
+var checkValidString = function (s) {
+  let [left, right, wild, rightWild] = [0, 0, 0, 0];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === ')') {
+      right += 1;
+    } else if (s[i] === '(') {
+      left += 1;
+    } else {
+      wild += 1;
+      rightWild += 1;
+    }
+
+    if (right > (left + wild)) return false;
+    while ((right + rightWild) > left) rightWild--;
+  }
+
+  return (right + rightWild) >= left;
+}
+
 // Cleaner
 var checkValidString = function (s) {
   let [left, right] = [0, 0]
